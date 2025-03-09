6293202b55edd00d0f74f8c1c04840fc736d3454
vkpdeveloper <vaibhavpathak9984@gmail.com>
2025-03-09 23:35:21 +0530

    feat(bun): initial bun app
    - update dev script to use watch flag
    - create basic server

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  port: 3000,
+  routes: {
+    "/search": new Response("Hello, world"),
+  },
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
